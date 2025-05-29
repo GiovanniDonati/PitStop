7b67433332f6e6f2b04bd12aa8c7a8a6c778ae11
GiovanniDonati <grdonati99@gmail.com>
2025-05-29 20:45:18 -0300

    feat: include router to pages

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,14 +1,27 @@
-import Dashboard from "./components/Dashboard/Dashboard";
-import Stock from "./components/Stock/Stock";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import SideBar from "./components/SideBar/SideBar";
+import Dashboard from "./pages/Dashboard";
+// import Agendamento from "./pages/Agendamento";
+// import Orcamento from "./pages/Orcamento";
+import Financeiro from "./pages/Financeiro";
+import Stock from "./pages/Stock";
 
 function App() {
   return (
-    <div className="w-full h-screen flex">
-      <SideBar />
-      {/* <Dashboard /> */}
-      <Stock />
-    </div>
+    <Router>
+      <div className="flex min-h-screen">
+        <SideBar />
+        <div className="flex-1">
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+            {/* <Route path="/agendamento" element={<Agendamento />} /> */}
+            {/* <Route path="/orcamento" element={<Orcamento />} /> */}
+            <Route path="/stock" element={<Stock />} />
+            <Route path="/financeiro" element={<Financeiro />} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
   );
 }
 
